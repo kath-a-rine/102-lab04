f9dac58092eec145ceb173c19cf75d97c22a8dd8
Katharine Swilley <kswilley@gmail.com>
2022-03-25 16:02:27 -0700

    js changes

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ function favShow () {
     } while (parseInt(answer) !== 6)
   
     if (parseInt(answer) === 6) {
-      document.write("It took you " + guesses + " times to guess the correct answer.")
+      document.write("It took you " + guesses + " time(s) to guess the correct answer.")
     }
   
   }
@@ -24,7 +24,7 @@ function favShow () {
     let rating = prompt("Scale of 1-5 how much do you love to eat corn?");
   
     for (let i = 0; i < rating; i++) {
-      output += "<img src='images/corn.jpg' />";
+      output += "<img src='/corn.jpg' />";
     }
   
     document.write(output);
