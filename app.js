7081d5696b5ff08d2cceb9a466e978c215c5808b
Katharine Swilley <kswilley@gmail.com>
2022-03-24 19:49:12 -0700

    javascript

--- app.js
+++ app.js
@@ -1,12 +1,32 @@
-function message() {
 
-    let show = prompt("What is your favorite show?");
-    let watch = prompt("What streaming service do you watch " + show +  " on?");
-    let pay = prompt("Do you pay for " + watch + "?")
-    if( pay === "yes") {
-        document.write("That's great! I hope you enjoy " + show + " and it is stays on " + watch + " forever!");
-    } else {
-        document.write("That's great! Although you really should start paying for " + watch + ". You don't want to get locked out of the account. Then you'll never be able to watch " + show + " again.");
+function favShow () {
+    let favShow = prompt("What is your favorite show?");
+    document.write("That's cool! "+ favShow + " deserved an Emmy!");
+  }
+  
+  function sopranos() {
+    let answer;
+    let guesses = 0;
+  
+    do {
+      guesses++;
+      answer = prompt("Guess how many seasons Sopranos was on the air");
+    } while (parseInt(answer) !== 6)
+  
+    if (parseInt(answer) === 6) {
+      document.write("It took you " + guesses + " times to guess the correct answer.")
     }
-
-}
+  
+  }
+  
+  function rating() {
+    let output = '';
+    let rating = prompt("Scale of 1-5 how much do you love to eat corn?");
+  
+    for (let i = 0; i < rating; i++) {
+      output += "<img src='images/corn.jpg' />";
+    }
+  
+    document.write(output);
+  
+  }
